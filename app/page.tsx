b38cfbb55ff9dcc0cc2ea72cfc6ac071f16a38ee
FRANCO CONDE <fnicolasconde@gmail.com>
2023-10-16 08:52:54 -0300

    background and header

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,41 @@
 
 export default function layout() {
+  
 
   return (
-    <><div className="bg-black w-full h-1/4 justify-center">
-      <div className="flex justify-center ">
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white border-solid px-8 py-3  font-medium">Trabajo</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white border-solid px-8 py-3  font-medium">Sobre mí</a>
-        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white border-solid px-8 py-3  font-medium">Contacto</a>
-      </div>
-    </div>
-    <div className="flex w-full my-48">
-      <div className="w-1/2 flex justify-center items-center">
-        <h1 className="text-white text-6xl"> Hola soy Ahazel</h1>
-      </div>
-      <div>
-        {/* Material grafico, gif o lo que sea */}
-      </div>
-    </div>
+    <>
     
+    <script src="https://kit.fontawesome.com/5cb5f3efa7.js" crossOrigin="anonymous"></script>
+    <div className=" w-full h-1/4 justify-center">
+      <div className="flex justify-center ">
+        <a href="#" className="text-blue-500    border-solid px-8 py-3  font-medium">Trabajo</a>
+        <a href="#" className="text-blue-500  border-solid px-8 py-3  font-medium">Sobre mí</a>
+        <a href="#" className="text-blue-500  border-solid px-8 py-3  font-medium">Contacto</a>
+      </div>
+      <hr></hr>
+    </div>
+      <div className="block my-48 justify-center items-center text-center">
+        <div className="">
+        <h1 className="text-6xl font-bold text-center">Yo soy <br></br> <a className="text-blue-500">Ahazel</a>, un gusto.</h1> 
+      </div> 
+       
+        <div className=" my-6">
+        <p className=" text-gray-400 font-semibold justify-center text-2xl">Desarrollo de aplicaciones de escritorio, aplicaciones web y servicios prácticos de ciberseguridad.</p> 
+      </div>
+      
+      <div className="flex justify-center">
+      <a href="https://github.com/ahazel17"  target="_blank"><i className="fa-brands fa-github fa-2xl px-3 py-3" ></i></a>
+      <a href="https://www.linkedin.com/in/condefranco/"  target="_blank"><i className="fa-brands fa-linkedin fa-2xl px-3 py-3"></i></a>
+      <a href="https://www.instagram.com/jojovoid_"  target="_blank"><i className="fa-brands fa-instagram fa-2xl px-3 py-3"></i></a>
+      </div>
+    </div>
+
+    <div className=" bg-gray-800 h-1/3 text-white">
+      <h1 className="text-3xl"> Mis trabajos hasta el momento...
+
+      </h1>
+
+    </div>
     
     
     
